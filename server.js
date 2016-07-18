1efba760a68ff363168e2c32d1aabff15d3116a7
sitaramyadav <sitaram@thoughworks.com>
2016-07-19 00:36:44 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var controller = require('./library/controller.js');
+var port = 8080;
+var server = http.createServer(controller);
+server.listen(port);
+console.log(server.on('error',function (argument) {
+	console.log(argument,'====argument');
+}),'===========');
